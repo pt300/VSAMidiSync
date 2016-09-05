08df2f56b64974988e024b302b73e66db8a22290
pt300 <pt300@tlen.pl>
2016-09-05 22:52:30 +0200

    sentinel attribute depending on compiler

--- AXCtrl.h
+++ AXCtrl.h
@@ -7,6 +7,15 @@
 
 #include <windows.h>
 #include <ocidl.h>
+/*
+ * totally not stoeled from glib
+ */
+
+#if     __GNUC__ >= 4
+#define GNUC_NULL_TERMINATED __attribute__((__sentinel__))
+#else
+#define GNUC_NULL_TERMINATED
+#endif
 
 typedef struct {
 	IPersistStreamInit *stream;
@@ -17,7 +26,7 @@ HRESULT AX_init(void);
 void AX_deinit(void);
 HRESULT AX_getControl(AX_object *obj, LPCOLESTR name);
 HRESULT AX_destroyControl(AX_object *obj);
-HRESULT AX_callMethod(AX_object *obj, OLECHAR *name, enum VARENUM ret_type, void *ret_val, ...) __attribute__((__sentinel__));
+HRESULT AX_callMethod(AX_object *obj, OLECHAR *name, enum VARENUM ret_type, void *ret_val, ...) GNUC_NULL_TERMINATED;
 HRESULT AX_setValue(AX_object *obj, OLECHAR *name, enum VARENUM type, void *var);
 
 #endif //AXCTRL_H
